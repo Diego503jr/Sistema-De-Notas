e6690818ab98cb242f8b3dc1ac3d262063ed00bd
DiegoCarias <diegocarias503@gmail>
2023-10-18 16:09:45 -0600

    Mostrando forms en un solo formulario subpadre

--- AdminForm.Designer.cs
+++ AdminForm.Designer.cs
@@ -41,7 +41,7 @@
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
             this.BarraTitulo.SuspendLayout();
             this.BarraMenuLateral.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.btnCerrarSesion)).BeginInit();
@@ -49,13 +49,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
             this.SuspendLayout();
             // 
             // BarraTitulo
             // 
             this.BarraTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(28)))), ((int)(((byte)(36)))));
-            this.BarraTitulo.Controls.Add(this.pictureBox1);
+            this.BarraTitulo.Controls.Add(this.btnCerrar);
             this.BarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
             this.BarraTitulo.Location = new System.Drawing.Point(0, 0);
             this.BarraTitulo.Name = "BarraTitulo";
@@ -87,7 +87,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(97, 438);
+            this.label1.Location = new System.Drawing.Point(97, 404);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(125, 24);
             this.label1.TabIndex = 10;
@@ -134,6 +134,7 @@
             this.btnMenuAlumnos.TabIndex = 3;
             this.btnMenuAlumnos.Text = "Alumnos";
             this.btnMenuAlumnos.UseVisualStyleBackColor = false;
+            this.btnMenuAlumnos.Click += new System.EventHandler(this.btnMenuAlumnos_Click);
             // 
             // btnMenuDocentes
             // 
@@ -162,7 +163,7 @@
             this.btnCerrarSesion.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.btnCerrarSesion.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btnCerrarSesion.Image = global::SistemaDeNotas.Properties.Resources.salir;
-            this.btnCerrarSesion.Location = new System.Drawing.Point(15, 412);
+            this.btnCerrarSesion.Location = new System.Drawing.Point(15, 378);
             this.btnCerrarSesion.Name = "btnCerrarSesion";
             this.btnCerrarSesion.Size = new System.Drawing.Size(76, 77);
             this.btnCerrarSesion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -205,18 +206,18 @@
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             // 
-            // pictureBox1
+            // btnCerrar
             // 
-            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox1.Image = global::SistemaDeNotas.Properties.Resources.cerrar;
-            this.pictureBox1.Location = new System.Drawing.Point(763, 5);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.btnCerrar.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrar.Image = global::SistemaDeNotas.Properties.Resources.cerrar;
+            this.btnCerrar.Location = new System.Drawing.Point(763, 6);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(25, 25);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 0;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // AdminForm
             // 
@@ -238,7 +239,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -246,11 +247,9 @@
         #endregion
 
         private System.Windows.Forms.Panel BarraTitulo;
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel BarraMenuLateral;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button btnMenuDocentes;
-        private System.Windows.Forms.Panel ContPrincipal;
         private System.Windows.Forms.PictureBox pictureBox5;
         private System.Windows.Forms.Button btnMenuCursos;
         private System.Windows.Forms.PictureBox pictureBox4;
@@ -259,5 +258,7 @@
         private System.Windows.Forms.Button btnMenuAlumnos;
         private System.Windows.Forms.PictureBox btnCerrarSesion;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel ContPrincipal;
+        private System.Windows.Forms.PictureBox btnCerrar;
     }
 }
--- AdminForm.cs
+++ AdminForm.cs
@@ -17,7 +17,32 @@ namespace SistemaDeNotas
             InitializeComponent();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void AbrirFormHijo(object formHijo)
+        {
+            if (this.ContPrincipal.Controls.Count > 0)
+            
+                this.ContPrincipal.Controls.RemoveAt(0);
+                Form fh = formHijo as Form;
+                //AlumnoFormSecundario alumnFormSec = new AlumnoFormSecundario();
+                fh.TopLevel = false;
+                fh.Dock = DockStyle.Fill;
+                this.ContPrincipal.Controls.Add(fh);
+                this.ContPrincipal.Tag = fh;
+                fh.Show();
+            
+        }
+
+        private void ContPrincipal_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnMenuAlumnos_Click(object sender, EventArgs e)
+        {
+            AbrirFormHijo(new AlumnoFormSecundario());
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
