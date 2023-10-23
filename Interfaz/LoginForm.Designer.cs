e310d1889b1eb0a2f1aa278eee1a5e594249adca
f207 <f207@STCF207010EF>
2023-10-23 15:26:04 +0200

    Agregando login docente y alumno form

---
+++ Interfaz/LoginForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace SistemaDeNotas
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BarraTitulo = new System.Windows.Forms.Panel();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDocenteLoginForm = new System.Windows.Forms.Button();
+            this.btnAlumnoLoginForm = new System.Windows.Forms.Button();
+            this.BarraTitulo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BarraTitulo
+            // 
+            this.BarraTitulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(237)))), ((int)(((byte)(28)))), ((int)(((byte)(36)))));
+            this.BarraTitulo.Controls.Add(this.btnCerrar);
+            this.BarraTitulo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.BarraTitulo.Location = new System.Drawing.Point(0, 0);
+            this.BarraTitulo.Margin = new System.Windows.Forms.Padding(2);
+            this.BarraTitulo.Name = "BarraTitulo";
+            this.BarraTitulo.Size = new System.Drawing.Size(600, 29);
+            this.BarraTitulo.TabIndex = 6;
+            this.BarraTitulo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.BarraTitulo_MouseDown);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrar.Image = global::SistemaDeNotas.Properties.Resources.cerrar;
+            this.btnCerrar.Location = new System.Drawing.Point(574, 6);
+            this.btnCerrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(19, 19);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 7;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(260, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Iniciar sesion como";
+            // 
+            // btnDocenteLoginForm
+            // 
+            this.btnDocenteLoginForm.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnDocenteLoginForm.Location = new System.Drawing.Point(104, 205);
+            this.btnDocenteLoginForm.Name = "btnDocenteLoginForm";
+            this.btnDocenteLoginForm.Size = new System.Drawing.Size(75, 23);
+            this.btnDocenteLoginForm.TabIndex = 8;
+            this.btnDocenteLoginForm.Text = "Docente";
+            this.btnDocenteLoginForm.UseVisualStyleBackColor = true;
+            this.btnDocenteLoginForm.Click += new System.EventHandler(this.btnDocenteLoginForm_Click);
+            // 
+            // btnAlumnoLoginForm
+            // 
+            this.btnAlumnoLoginForm.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAlumnoLoginForm.Location = new System.Drawing.Point(438, 205);
+            this.btnAlumnoLoginForm.Name = "btnAlumnoLoginForm";
+            this.btnAlumnoLoginForm.Size = new System.Drawing.Size(75, 23);
+            this.btnAlumnoLoginForm.TabIndex = 9;
+            this.btnAlumnoLoginForm.Text = "Alumno";
+            this.btnAlumnoLoginForm.UseVisualStyleBackColor = true;
+            this.btnAlumnoLoginForm.Click += new System.EventHandler(this.btnAlumnoLoginForm_Click);
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(600, 344);
+            this.Controls.Add(this.btnAlumnoLoginForm);
+            this.Controls.Add(this.btnDocenteLoginForm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BarraTitulo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "LoginForm";
+            this.Text = "Inicio de Sesión";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.BarraTitulo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel BarraTitulo;
+        private System.Windows.Forms.PictureBox btnCerrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDocenteLoginForm;
+        private System.Windows.Forms.Button btnAlumnoLoginForm;
+    }
+}
+
--- Interfaz/LoginForm.cs
+++ Interfaz/LoginForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaDeNotas.Interfaz;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -44,6 +45,20 @@ namespace SistemaDeNotas
             //}
 
             
+        }
+
+        private void btnDocenteLoginForm_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            DocenteLogin docenteLogin = new DocenteLogin();
+            docenteLogin.ShowDialog();
+        }
+
+        private void btnAlumnoLoginForm_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AlumnoLogin alumnoLogin = new AlumnoLogin();
+            alumnoLogin.ShowDialog();
         }
     }
 }
